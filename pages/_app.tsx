bf4d38eebfffe4db11be09a2cec338ba4dcf19b7
sandeepsharma09 <sandeepsharma999000@gmail.com>
2023-07-19 12:25:13 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,305 @@
+import AnalyticsProvider, {
+  initializeAnalytics,
+} from 'components/AnalyticsProvider'
+initializeAnalytics()
+import "./../styles/global.css";
+import { Inter } from '@next/font/google'
+import type { AppContext, AppProps } from 'next/app'
+import { default as NextApp } from 'next/app'
+import { ThemeProvider, useTheme } from 'next-themes'
+import { darkTheme, globalReset } from 'stitches.config'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+
+  getDefaultWallets,
+
+  darkTheme as rainbowDarkTheme,
+  lightTheme as rainbowLightTheme,
+} from '@rainbow-me/rainbowkit'
+
+import * as Tooltip from '@radix-ui/react-tooltip'
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+
+import {
+  ReservoirKitProvider,
+  darkTheme as reservoirDarkTheme,
+  lightTheme as reservoirLightTheme,
+  ReservoirKitTheme,
+  CartProvider,
+} from '@reservoir0x/reservoir-kit-ui'
+import { FC, useEffect, useState } from 'react'
+import { HotkeysProvider } from 'react-hotkeys-hook'
+import ToastContextProvider from 'context/ToastContextProvider'
+import supportedChains from 'utils/chains'
+import { useMarketplaceChain } from 'hooks'
+import ChainContextProvider from 'context/ChainContextProvider'
+
+import { Mumbai, Goerli,Ethereum, Arbitrum, ArbitrumGoerli } from "@thirdweb-dev/chains";
+////////////start/////////////
+
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+  useSigner,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  connectorsForWallets,
+  wallet,
+  RainbowKitProvider,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ThirdwebSDKProvider, ChainId } from "@thirdweb-dev/react";
+
+////////////end/////////////
+//CONFIGURABLE: Use nextjs to load your own custom font: https://nextjs.org/docs/basic-features/font-optimization
+const inter = Inter({
+  subsets: ['latin'],
+})
+
+export const NORMALIZE_ROYALTIES = process.env.NEXT_PUBLIC_NORMALIZE_ROYALTIES
+  ? process.env.NEXT_PUBLIC_NORMALIZE_ROYALTIES === 'true'
+  : false
+
+// const { chains, provider } = configureChains(supportedChains, [
+//   alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || '' }),
+//   publicProvider(),
+// ])
+
+// const { connectors } = getDefaultWallets({
+//   appName: 'Reservoir Marketplace',
+//   chains,
+// })
+
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors,
+//   provider,
+// })
+
+//CONFIGURABLE: Here you can override any of the theme tokens provided by RK: https://docs.reservoir.tools/docs/reservoir-kit-theming-and-customization
+const reservoirKitThemeOverrides = {
+  headlineFont: inter.style.fontFamily,
+  font: inter.style.fontFamily,
+  primaryColor: '#6E56CB',
+  primaryHoverColor: '#644fc1',
+}
+
+
+////////////////////////start/////////////////
+
+
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth_goerli",
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.metaMask({ chains, shimDisconnect: true }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: "probably nothing", chains }),
+      wallet.rainbow({ chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [
+      wallet.argent({ chains }),
+      wallet.brave({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.imToken({ chains }),
+      wallet.injected({
+        chains,
+        shimDisconnect: true,
+      }),
+      wallet.ledger({
+        chains,
+      }),
+      wallet.steak({ chains }),
+      wallet.trust({ chains, shimDisconnect: true }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+const activeChainId = ChainId.Goerli;
+function ThirdwebProvider({ wagmiClient, children }: any) {
+  const { data: signer } = useSigner();
+  return (
+    <ThirdwebSDKProvider
+      desiredChainId={activeChainId}
+      signer={signer as any}
+      provider={wagmiClient.provider}
+      queryClient={wagmiClient.queryClient as any}
+    >
+      {children}
+    </ThirdwebSDKProvider>
+  );
+}
+////////////////////////end/////////////////
+function AppWrapper(props: AppProps & { baseUrl: string }) {
+
+  const activeChain = "mumbai"; 
+  const activeChain1 = "goerli"; 
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      value={{
+        dark: darkTheme.className,
+        light: 'light',
+      }}
+    >
+      {/* // activeChain={activeChain} */}
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+          <ThirdwebProvider wagmiClient={wagmiClient}>
+        <ChainContextProvider>
+          
+          <AnalyticsProvider>
+            <MyApp {...props} />
+          </AnalyticsProvider>
+          
+        </ChainContextProvider>
+        </ThirdwebProvider>
+      </RainbowKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
+  )
+}
+
+function MyApp({
+  Component,
+  pageProps,
+  baseUrl,
+}: AppProps & { baseUrl: string }) {
+  globalReset()
+
+  const { theme } = useTheme()
+  const marketplaceChain = useMarketplaceChain()
+  const [reservoirKitTheme, setReservoirKitTheme] = useState<
+    ReservoirKitTheme | undefined
+  >()
+
+  const [rainbowKitTheme, setRainbowKitTheme] = useState<
+    | ReturnType<typeof rainbowDarkTheme>
+    | ReturnType<typeof rainbowLightTheme>
+    | undefined
+  >()
+
+  useEffect(() => {
+    if (theme == 'dark') {
+      setReservoirKitTheme(reservoirDarkTheme(reservoirKitThemeOverrides))
+      setRainbowKitTheme(
+        rainbowDarkTheme({
+          borderRadius: 'small',
+        })
+      )
+    } else {
+      setReservoirKitTheme(reservoirLightTheme(reservoirKitThemeOverrides))
+      setRainbowKitTheme(
+        rainbowLightTheme({
+          borderRadius: 'small',
+        })
+      )
+    }
+  }, [theme])
+
+  const FunctionalComponent = Component as FC
+
+  let source = process.env.NEXT_PUBLIC_MARKETPLACE_SOURCE
+
+  if (!source && process.env.NEXT_PUBLIC_HOST_URL) {
+    try {
+      const url = new URL(process.env.NEXT_PUBLIC_HOST_URL)
+      source = url.host
+    } catch (e) {}
+  }
+
+  return (
+    <HotkeysProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        value={{
+          dark: darkTheme.className,
+          light: 'light',
+        }}
+      >
+        <ReservoirKitProvider
+          options={{
+            //CONFIGURABLE: Override any configuration available in RK: https://docs.reservoir.tools/docs/reservoirkit-ui#configuring-reservoirkit-ui
+            // Note that you should at the very least configure the source with your own domain
+            chains: supportedChains.map(({ proxyApi, id }) => {
+              return {
+                id,
+                baseApiUrl: `${baseUrl}${proxyApi}`,
+                default: marketplaceChain.id === id,
+              }
+            }),
+            source: source,
+            normalizeRoyalties: NORMALIZE_ROYALTIES,
+          }}
+          theme={reservoirKitTheme}
+        >
+          <CartProvider>
+            <Tooltip.Provider>
+              <RainbowKitProvider
+                chains={chains}
+                theme={rainbowKitTheme}
+                modalSize="compact"
+              >
+                <ToastContextProvider>
+                  <FunctionalComponent {...pageProps} />
+                </ToastContextProvider>
+              </RainbowKitProvider>
+            </Tooltip.Provider>
+          </CartProvider>
+        </ReservoirKitProvider>
+      </ThemeProvider>
+    </HotkeysProvider>
+  )
+}
+
+AppWrapper.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await NextApp.getInitialProps(appContext)
+  let baseUrl = ''
+
+  if (appContext.ctx.req?.headers.host) {
+    const host = appContext.ctx.req?.headers.host
+    baseUrl = `${host.includes('localhost') ? 'http' : 'https'}://${host}`
+  } else if (process.env.NEXT_PUBLIC_HOST_URL) {
+    baseUrl = process.env.NEXT_PUBLIC_HOST_URL || ''
+  }
+  baseUrl = baseUrl.replace(/\/$/, '')
+
+  return { ...appProps, baseUrl }
+}
+
+export default AppWrapper
